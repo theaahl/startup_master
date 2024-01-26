af0a09f1a8791a601343217cc144fc2812d48a10
helenfs <helenfs@stud.ntnu.no>
2024-01-26 12:39:24 +0100

    Commented out more tests

--- app_test.py
+++ app_test.py
@@ -25,22 +25,22 @@ def create_chat_completion(response: str, role: str = "assistant") -> ChatComple
     )
 
 
-@patch("openai.resources.chat.Completions.create")
-def test_Chatbot(openai_create):
-    at = AppTest.from_file("Chatbot.py").run()
-    assert not at.exception
-    at.chat_input[0].set_value("Do you know any jokes?").run()
-    assert at.info[0].value == "Please add your OpenAI API key to continue."
+# @patch("openai.resources.chat.Completions.create")
+# def test_Chatbot(openai_create):
+#     at = AppTest.from_file("Chatbot.py").run()
+#     assert not at.exception
+#     at.chat_input[0].set_value("Do you know any jokes?").run()
+#     assert at.info[0].value == "Please add your OpenAI API key to continue."
 
-    JOKE = "Why did the chicken cross the road? To get to the other side."
-    openai_create.return_value = create_chat_completion(JOKE)
-    at.text_input(key="chatbot_api_key").set_value("sk-...")
-    at.chat_input[0].set_value("Do you know any jokes?").run()
-    print(at)
-    assert at.chat_message[1].markdown[0].value == "Do you know any jokes?"
-    assert at.chat_message[2].markdown[0].value == JOKE
-    assert at.chat_message[2].avatar == "assistant"
-    assert not at.exception
+#     JOKE = "Why did the chicken cross the road? To get to the other side."
+#     openai_create.return_value = create_chat_completion(JOKE)
+#     at.text_input(key="chatbot_api_key").set_value("sk-...")
+#     at.chat_input[0].set_value("Do you know any jokes?").run()
+#     print(at)
+#     assert at.chat_message[1].markdown[0].value == "Do you know any jokes?"
+#     assert at.chat_message[2].markdown[0].value == JOKE
+#     assert at.chat_message[2].avatar == "assistant"
+#     assert not at.exception
 
 
 # @patch("langchain.llms.OpenAI.__call__")
